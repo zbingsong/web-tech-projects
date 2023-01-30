121488926c278449f9d03fdffbcd015d137cac90
Bingsong Zeng <bingsong@usc.edu>
2023-01-29 16:40:42 -0800

    working on sending requests to external apis

--- otO9ZoER_hw6/app.py
+++ otO9ZoER_hw6/app.py
@@ -1,25 +1,40 @@
-from flask import Flask, render_template, request
+from flask import Flask, request, Response, send_from_directory
 from werkzeug.datastructures import ImmutableMultiDict
+import requests
 
 
 app: Flask = Flask(__name__)
 
 @app.get('/')
-def index() -> str:
-  return render_template('index.html')
+def index() -> Response:
+  return send_from_directory('static', 'index.html')
 
-@app.get('/event/<str: event_id>')
+# @app.get('/index.css')
+# def get_css() -> Response:
+#   return send_from_directory('static', 'index.css')
+
+# @app.get('/index.js')
+# def get_js() -> Response:
+#   return send_from_directory('static', 'index.js')
+
+@app.get('/<path:filename>')
+def get_static_file(filename: str) -> Response:
+  return send_from_directory('static', filename)
+
+
+@app.get('/event/<string:event_id>')
 def get_event_detail(event_id: str):
-  return ''
+  return 'event' + event_id
 
-@app.get('/venue/<str: venue_id>')
-def get_event_detail(venue_id: str):
-  return ''
+@app.get('/venue/<string:venue_id>')
+def get_venue_detail(venue_id: str):
+  return 'venue' + venue_id
 
-@app.post('/search')
+@app.get('/search')
 def search_events():
-  data: ImmutableMultiDict = request.form
-  return ''
+  params: ImmutableMultiDict[str, str] = request.args
+  
+  return 'search event'
 
 
 if __name__ == '__main__':
