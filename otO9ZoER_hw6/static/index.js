81712fcbb85975a68cf9fde69f5ede7014609ec8
Bingsong Zeng <bingsong@usc.edu>
2023-01-30 12:20:22 -0800

    changed fetch().then() chain to async/await for cleaner code

--- otO9ZoER_hw6/static/index.js
+++ otO9ZoER_hw6/static/index.js
@@ -1,9 +1,9 @@
 const IPINFO_API_KEY = '4c616ba7471362';
-const GOOGLE_GEOCODING_API_KEY = '';
-const TICKETMASTER_API_KEY = '';
+const GOOGLE_GEOCODING_API_KEY = 'AIzaSyAzeFEsfPKDhdxHcwYkDuDirDo4IAifTfk';
+const TICKETMASTER_API_KEY = 'kokaaLQxqCux3XHdGCnVGD2zh6mODd2r';
 
 
-function searchEvents(event) {
+async function searchEvents(event) {
   console.log('search button clicked');
   event.preventDefault();
   const formData = new FormData(event.target);
@@ -11,8 +11,23 @@ function searchEvents(event) {
   const distance = 
     formData.get('distance') === '' ? 10 : parseInt(formData.get('distance'));
   const category = formData.get('category');
-  const location = getLocation(formData);
-  console.log(`${keyword}, ${distance}, ${category}, ${location}`);
+  const location = await getLocation(formData);
+  console.log(`${keyword}, ${distance}, ${category}, ${formData.get('location-checkbox')}, ${JSON.stringify(location)}`);
+
+  try {
+    const response = await fetch(
+      `/search?keyword=${keyword}&distance=${distance}&category=${category}&lng=${location.lng}&lat=${location.lat}`
+    );
+    const data = await response.json();
+    console.log(data);
+    if (data.events === undefined) {
+      throw new Error('Failure in retrieving events');
+    }
+    showResults(data.events);
+  } catch (error) {
+    console.log(error);
+    alert(JSON.stringify(error));
+  }
 }
 
 
@@ -29,12 +44,37 @@ function clearForm(event) {
 }
 
 
-function getLocation(formData) {
-  if (formData.get('location-checkbox')) {
-
+async function getLocation(formData) {
+  const coordinates = { lng: 0, lat: 0 }
+  if (formData.get('location-checkbox') === 'on') {
+    console.log('ipinfo');
+    try {
+      const response = await fetch(`https://ipinfo.io/?token=${IPINFO_API_KEY}`);
+      const data = await response.json();
+      const coordArr = data.loc.split(',');
+      coordinates.lat = parseFloat(coordArr[0]);
+      coordinates.lng = parseFloat(coordArr[1]);
+    } catch (error) {
+      console.log('location error');
+      alert(JSON.stringify(error));
+    }
   } else {
+    console.log('google geocoding');
     const locationInput = formData.get('location');
+    try {
+      const response = await fetch(
+        `https://maps.googleapis.com/maps/api/geocode/json?address=${locationInput}&key=${GOOGLE_GEOCODING_API_KEY}`
+      );
+      const data = await response.json();
+      const coordObj = data.results[0].geometry.location;
+      coordinates.lat = coordObj.lat;
+      coordinates.lng = coordObj.lng;
+    } catch (error) {
+      console.log('location error');
+      alert(JSON.stringify(error));
+    }
   }
+  return coordinates;
 }
 
 
@@ -45,8 +85,20 @@ function toggleLocationInput(event) {
 }
 
 
+/**
+ * 
+ * @param {Array<{
+ *  id: string, 
+ *  date: string, 
+ *  time: string, 
+ *  image_url: string, 
+ *  name: string, 
+ *  genre: string, 
+ *  venue: string
+ * }>} results 
+ */
 function showResults(results) {
-  if (results.length === 0) {
+  if (!results || results.length === 0) {
     document.getElementById('search-no-result').style.display = 'block';
     document.getElementById('search-result').style.display = 'none';
     // the following four lines are adapted from @Fernando and @Nathan's answer at: 
@@ -61,15 +113,17 @@ function showResults(results) {
       const row = document.createElement('tr');
 
       const date = document.createElement('th');
-      const dateText = document.createElement('span');
-      dateText.innerHTML = result.dates.localDate;
-      const timeText = document.createElement('span');
-      timeText.innerHTML = result.dates.localTime;
+      date.dataset.eventId = result.id;
+      const dateText = document.createElement('p');
+      dateText.innerHTML = result.date;
+      const timeText = document.createElement('p');
+      timeText.innerHTML = result.time;
       date.append(dateText, timeText);
 
       const icon = document.createElement('th');
       const iconImg = document.createElement('img');
-      iconImg.src = result.images;
+      iconImg.src = result.image_url;
+      iconImg.className = 'search-result-event-icon';
       icon.append(iconImg);
 
       const event = document.createElement('th');
@@ -77,10 +131,10 @@ function showResults(results) {
       event.onclick = showEventDetail(result.id);
 
       const genre = document.createElement('th');
-      genre.innerHTML = result.segment;
+      genre.innerHTML = result.genre;
 
       const venue = document.createElement('th');
-      venue.innerHTML = result.venue.name;
+      venue.innerHTML = result.venue;
 
       row.append(date, icon, event, genre, venue);
       resultTableBody.append(row);
