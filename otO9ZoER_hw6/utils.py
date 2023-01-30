a5d6e8ec9eb012897acf8e2fffa78563d9de0503
Bingsong Zeng <bingsong@usc.edu>
2023-01-29 21:34:05 -0800

    moved ticketmaster api key to this file and created a dict for getting segmentId

---
+++ otO9ZoER_hw6/utils.py
@@ -0,0 +1,14 @@
+from typing import Final
+
+
+TICKETMASTER_API_KEY: Final[str] = 'kokaaLQxqCux3XHdGCnVGD2zh6mODd2r'
+
+CATEGORY_OPTIONS: Final[dict] = {
+  'Default': '',
+  'Music': '&segmentId=KZFzniwnSyZfZ7v7nJ',
+  'Sports': '&segmentId=KZFzniwnSyZfZ7v7nE',
+  'Arts': '&segmentId=KZFzniwnSyZfZ7v7na',
+  'Theatre': '&segmentId=KZFzniwnSyZfZ7v7na',
+  'Film': '&segmentId=KZFzniwnSyZfZ7v7nn',
+  'Miscellaneous': '&segmentId=KZFzniwnSyZfZ7v7n1',
+}
