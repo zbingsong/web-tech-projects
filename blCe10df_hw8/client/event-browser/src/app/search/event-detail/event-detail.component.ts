c7852a6aa3d605d563367bc56c0288b53b3d119d
Bingsong Zeng <bingsong@usc.edu>
2023-03-09 22:36:48 -0800

    working on this component

--- blCe10df_hw8/client/event-browser/src/app/search/event-detail/event-detail.component.ts
+++ blCe10df_hw8/client/event-browser/src/app/search/event-detail/event-detail.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { AppService } from 'src/app/app.service';
+import {
+  EventDetail,
+  EventDetailData,
+} from 'src/app/common/event-detail.interface';
 
 @Component({
   selector: 'app-event-detail',
   templateUrl: './event-detail.component.html',
-  styleUrls: ['./event-detail.component.css']
+  styleUrls: ['./event-detail.component.css'],
 })
-export class EventDetailComponent {
+export class EventDetailComponent implements OnInit, OnDestroy {
+  private subscription?: Subscription;
+  public eventDetail?: EventDetail;
 
+  constructor(private readonly service: AppService) {}
+
+  public ngOnInit(): void {
+    this.subscription = this.service.eventDetailSubj$.subscribe(
+      (data: EventDetailData) => {
+        if (data.event_detail !== undefined) {
+          this.eventDetail = data.event_detail;
+        }
+      },
+    );
+  }
+
+  public ngOnDestroy(): void {
+    this.subscription?.unsubscribe();
+  }
 }
