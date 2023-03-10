47fef92ef320bae505db11c5042b9d1583e381eb
Bingsong Zeng <bingsong@usc.edu>
2023-03-09 22:36:27 -0800

    linked values of boolean observables in service to display of components

--- blCe10df_hw8/client/event-browser/src/app/search/search.component.ts
+++ blCe10df_hw8/client/event-browser/src/app/search/search.component.ts
@@ -1,16 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { AppService } from '../app.service';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.css'],
 })
-export class SearchComponent {
+export class SearchComponent implements OnInit, OnDestroy {
   public ifShowSearchResult: boolean = false;
   public ifShowEventDetail: boolean = false;
   public ifShowVenueDetail: boolean = false;
+  private readonly subscription: Subscription = new Subscription();
 
-  public showSearchResult(): void {
-    this.ifShowSearchResult = true;
+  constructor(private readonly service: AppService) {}
+
+  public ngOnInit(): void {
+    this.subscription.add(
+      this.service.ifShowSearchResult$.subscribe((data: boolean) => {
+        this.ifShowSearchResult = data;
+      }),
+    );
+    this.subscription.add(
+      this.service.ifShowEventDetail$.subscribe((data: boolean) => {
+        this.ifShowEventDetail = data;
+      }),
+    );
+    this.subscription.add(
+      this.service.ifShowVenueDetail$.subscribe((data: boolean) => {
+        this.ifShowVenueDetail = data;
+      }),
+    );
+  }
+
+  public ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 }
