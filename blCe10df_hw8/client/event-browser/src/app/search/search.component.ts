251bce56655cf79b12e0b3af548302683f4eee4f
Bingsong Zeng <bingsong@usc.edu>
2023-03-16 22:37:26 -0700

    formatting

--- blCe10df_hw8/client/event-browser/src/app/search/search.component.ts
+++ blCe10df_hw8/client/event-browser/src/app/search/search.component.ts
@@ -1,39 +1,8 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Subscription } from 'rxjs';
-import { AppService } from '../app.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.css'],
 })
-export class SearchComponent implements OnInit, OnDestroy {
-  public ifShowSearchResult: boolean = false;
-  public ifShowEventDetail: boolean = false;
-  public ifShowVenueDetail: boolean = false;
-  private readonly subscription: Subscription = new Subscription();
-
-  constructor(private readonly service: AppService) {}
-
-  public ngOnInit(): void {
-    this.subscription.add(
-      this.service.ifShowSearchResult$.subscribe((data: boolean) => {
-        this.ifShowSearchResult = data;
-      }),
-    );
-    this.subscription.add(
-      this.service.ifShowEventDetail$.subscribe((data: boolean) => {
-        this.ifShowEventDetail = data;
-      }),
-    );
-    this.subscription.add(
-      this.service.ifShowVenueDetail$.subscribe((data: boolean) => {
-        this.ifShowVenueDetail = data;
-      }),
-    );
-  }
-
-  public ngOnDestroy(): void {
-    this.subscription.unsubscribe();
-  }
-}
+export class SearchComponent {}
