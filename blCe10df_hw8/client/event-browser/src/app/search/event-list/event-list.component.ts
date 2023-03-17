b96af8349d8c34453e06745d404a6c339e4a44e8
Bingsong Zeng <bingsong@usc.edu>
2023-03-16 22:39:45 -0700

    building component

--- blCe10df_hw8/client/event-browser/src/app/search/event-list/event-list.component.ts
+++ blCe10df_hw8/client/event-browser/src/app/search/event-list/event-list.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { AppService } from 'src/app/app.service';
-import { EventInfo, EventInfoData } from 'src/app/common/event-info.interface';
+import { EventInfo } from 'src/app/common/event-info.interface';
 
 @Component({
   selector: 'app-event-list',
@@ -9,41 +9,52 @@ import { EventInfo, EventInfoData } from 'src/app/common/event-info.interface';
   styleUrls: ['./event-list.component.css'],
 })
 export class EventListComponent implements OnInit, OnDestroy {
-  private subscription?: Subscription;
+  public subscription: Subscription = new Subscription();
   public eventList: EventInfo[] = [];
-  private sortingDirection = -1;
+  public ifSearched: boolean = false;
+  public ifResultList: boolean = true;
+  private sortingDirection: number = -1;
 
   constructor(private readonly service: AppService) {}
 
   public ngOnInit(): void {
-    this.subscription = this.service.eventInfoSubj$.subscribe(
-      (data: EventInfoData) => {
-        if (data.events !== undefined) {
-          this.eventList = data.events;
-          // sort by date and then by time ascending
-          this.eventList.sort((event1: EventInfo, event2: EventInfo) => {
-            let res: number = event1.date.localeCompare(event2.date);
-            if (res === 0) {
-              res = event1.time.localeCompare(event2.time);
-            }
-            return res;
-          });
-        }
-      },
+    this.subscription.add(
+      this.service.eventInfoSubj$.subscribe((data: EventInfo[]) => {
+        // sort by date and then by time ascending
+        data.sort((event1: EventInfo, event2: EventInfo) => {
+          let res: number = event1.date.localeCompare(event2.date);
+          if (res === 0) {
+            res = event1.time.localeCompare(event2.time);
+          }
+          return res;
+        });
+        this.eventList = data;
+      }),
+    );
+    this.subscription.add(
+      this.service.ifSearched$.subscribe((status: boolean) => {
+        this.ifSearched = !status;
+      }),
+    );
+    this.subscription.add(
+      this.service.ifResultList$.subscribe((status: boolean) => {
+        this.ifResultList = !status;
+      }),
     );
   }
 
   public ngOnDestroy(): void {
-    this.subscription?.unsubscribe();
+    this.subscription.unsubscribe();
   }
 
   public showEventDetail(eventId: string): void {
     this.service.getEventDetail(eventId);
+    this.service.ifResultList$.next(false);
   }
 
   public sortTable(key: keyof EventInfo): void {
+    this.sortingDirection = -this.sortingDirection;
     this.eventList.sort((event1: EventInfo, event2: EventInfo) => {
-      this.sortingDirection = -this.sortingDirection;
       return this.sortingDirection * event1[key].localeCompare(event2[key]);
     });
   }
