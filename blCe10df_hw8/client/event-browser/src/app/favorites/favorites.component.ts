36ff495dfcf6a42632cfce9710c18a79e062569f
Bingsong Zeng <bingsong@usc.edu>
2023-03-18 18:08:48 -0700

    finished working on this component

--- blCe10df_hw8/client/event-browser/src/app/favorites/favorites.component.ts
+++ blCe10df_hw8/client/event-browser/src/app/favorites/favorites.component.ts
@@ -10,12 +10,17 @@ export class FavoritesComponent implements OnInit {
   public favoriteEvents: EventInfoFav[] = [];
 
   public ngOnInit(): void {
-    this.favoriteEvents = Object.values<EventInfoFav>(localStorage);
-    console.log(this.favoriteEvents);
+    this.favoriteEvents = Object.values<string>(localStorage).map(
+      (eventString: string) => JSON.parse(eventString),
+    );
+    // console.log(this.favoriteEvents);
   }
 
   public removeEvent(eventId: string): void {
     localStorage.removeItem(eventId);
     alert('Removed from favorites!');
+    this.favoriteEvents = this.favoriteEvents.filter(
+      (event) => event.id !== eventId,
+    );
   }
 }
