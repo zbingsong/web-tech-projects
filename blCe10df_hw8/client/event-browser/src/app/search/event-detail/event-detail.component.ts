7660fa555c1c603a8b7df7950414dd30c8230b38
Bingsong Zeng <bingsong@usc.edu>
2023-03-19 00:37:31 -0700

    finished working on event title and favorite button

--- blCe10df_hw8/client/event-browser/src/app/search/event-detail/event-detail.component.ts
+++ blCe10df_hw8/client/event-browser/src/app/search/event-detail/event-detail.component.ts
@@ -11,20 +11,56 @@ import { EventInfoFav } from 'src/app/common/event-info-fav.interface';
 })
 export class EventDetailComponent implements OnInit, OnDestroy {
   public subscription: Subscription = new Subscription();
-  public eventDetail?: EventDetail;
-  public ifSearched: boolean = false;
-  public ifResultList: boolean = true;
+  public eventDetail: EventDetail = {
+    id: 'vvG1IZ9pNeVNoR',
+    name: 'P!NK: Summer Carnival 2023; P!NK: Summer Carnival 2023',
+    date: '2023-10-05',
+    time: '18:30:00',
+    artists: [
+      {
+        name: 'P!NK',
+        category: 'Music',
+      },
+      {
+        name: 'Pat Benatar & Neil Giraldo',
+        category: 'Music',
+      },
+      {
+        name: 'Grouplove',
+        category: 'Music',
+      },
+      {
+        name: 'KidCutUp',
+        category: 'Music',
+      },
+    ],
+    genre: 'Music | Rock | Pop',
+    category: 'Music',
+    venue_id: 'KovZ917ACh0',
+    venue: 'SoFi Stadium',
+    price: '55.95 - 405.95 USD',
+    status: 'On Sale',
+    status_color: 'green',
+    // eslint-disable-next-line max-len
+    buy: 'https://www.ticketmaster.com/pnk-summer-carnival-2023-inglewood-california-10-05-2023/event/0A005D68C2D2346F',
+    seatmap:
+      // eslint-disable-next-line max-len
+      'https://maps.ticketmaster.com/maps/geometry/3/event/0A005D68C2D2346F/staticImage?type=png&systemId=HOST',
+  };
+  public ifSearched: boolean = true;
+  public ifResultList: boolean = false;
   public addedToFavorites: boolean = false;
 
   constructor(private readonly service: AppService) {}
 
   public ngOnInit(): void {
-    console.log('event detail init');
+    // console.log('event detail init');
     this.subscription.add(
       this.service.eventDetailSubj$.subscribe((data: EventDetail) => {
-        console.log('event detail subscribed');
+        // console.log('event detail subscribed');
         this.eventDetail = data;
-        this.addedToFavorites = localStorage.getItem(data.id) === null;
+        this.addedToFavorites = localStorage.getItem(data.id) !== null;
+        console.log(`added to fav? ${this.addedToFavorites.toString()}`);
       }),
     );
     this.subscription.add(
@@ -54,9 +90,8 @@ export class EventDetailComponent implements OnInit, OnDestroy {
     const eventInfoFav: EventInfoFav = {
       id: this.eventDetail.id,
       date: this.eventDetail.date,
-      time: this.eventDetail.time,
       name: this.eventDetail.name,
-      category: this.eventDetail.category,
+      category: this.eventDetail.genre,
       venue: this.eventDetail.venue,
     };
     localStorage.setItem(this.eventDetail.id, JSON.stringify(eventInfoFav));
