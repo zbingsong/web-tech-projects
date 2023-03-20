562fa9718fcd405bc1eee0c13231d2ae95a76da2
Bingsong Zeng <bingsong@usc.edu>
2023-03-19 23:03:01 -0700

    removed testing data

--- blCe10df_hw8/client/event-browser/src/app/search/event-detail/event-dashboard/event-dashboard.component.ts
+++ blCe10df_hw8/client/event-browser/src/app/search/event-detail/event-dashboard/event-dashboard.component.ts
@@ -9,44 +9,8 @@ import { EventDetail } from 'src/app/common/event-detail.interface';
   styleUrls: ['./event-dashboard.component.css'],
 })
 export class EventDashboardComponent implements OnInit, OnDestroy {
-  public eventDetail: EventDetail = {
-    id: 'vvG1IZ9pNeVNoR',
-    name: 'P!NK: Summer Carnival 2023',
-    date: '2023-10-05',
-    time: '18:30:00',
-    artists: [
-      {
-        name: 'P!NK',
-        category: 'Music',
-      },
-      {
-        name: 'Pat Benatar & Neil Giraldo',
-        category: 'Music',
-      },
-      {
-        name: 'Grouplove',
-        category: 'Music',
-      },
-      {
-        name: 'KidCutUp',
-        category: 'Music',
-      },
-    ],
-    genre: 'Music | Rock | Pop',
-    category: 'Music',
-    venue_id: 'KovZ917ACh0',
-    venue: 'SoFi Stadium',
-    price: '55.95 - 405.95 USD',
-    status: 'On Sale',
-    status_color: 'green',
-    // eslint-disable-next-line max-len
-    buy: 'https://www.ticketmaster.com/pnk-summer-carnival-2023-inglewood-california-10-05-2023/event/0A005D68C2D2346F',
-    seatmap:
-      // eslint-disable-next-line max-len
-      'https://maps.ticketmaster.com/maps/geometry/3/event/0A005D68C2D2346F/staticImage?type=png&systemId=HOST',
-  };
-  // eslint-disable-next-line max-len
-  public artistsText: string = 'P!NK | Pat Benatar & Neil Giraldo | Grouplove | KidCutUp';
+  public eventDetail?: EventDetail;
+  public artistsText: string = '';
   public fbShareLink: string = '';
   public twShareLink: string = '';
   private subscription?: Subscription;
